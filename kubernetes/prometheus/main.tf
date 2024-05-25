0184ea525e09966e962dea93a57f193768593781
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2024-05-25 16:12:31 +0000

    Add Prometheus

---
+++ kubernetes/prometheus/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+  }
+}
+resource "helm_release" "prometheus" {
+  name             = "prometheus"
+  chart            = "kube-prometheus-stack"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  version          = "48.4.0"
+  namespace        = var.namespace
+  create_namespace = true
+  values = [
+    templatefile("${path.module}/templates/values.yaml", {
+      enable_alertmanager        = var.enable_alertmanager
+      alert_receiver_name        = var.alert_receiver_name
+      alert_receiver_url         = var.alert_receiver_url
+      alert_receiver_username    = var.alert_receiver_username
+      alert_receiver_password    = var.alert_receiver_password
+      prometheus_volume_size     = var.prometheus_volume_size
+      metrics_retention_duration = var.metrics_retention_duration
+      metrics_scrape_interval    = var.metrics_scrape_interval
+      kubernetes_cluster_name    = var.kubernetes_cluster_name
+      certificate_issuer_name    = var.certificate_issuer_name
+    })
+  ]
+}
+
+#
+# metrics server
+#
+
+resource "helm_release" "metrics_server" {
+  depends_on = [helm_release.prometheus]
+  name       = "metrics-server"
+  chart      = "metrics-server"
+  version    = "3.11.0"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  namespace  = var.namespace
+
+  set {
+    name  = "metrics.enabled"
+    value = true
+  }
+  set {
+    name  = "args"
+    value = "{--kubelet-insecure-tls}"
+  }
+}
---
+++ kubernetes/prometheus/variables.tf
@@ -0,0 +1,65 @@
+variable "enable_alertmanager" {
+  default     = false
+  type        = bool
+  description = "Enable Alertmanager"
+}
+
+variable "alert_receiver_name" {
+  default     = ""
+  type        = string
+  description = "Name of the alertmanager receiver"
+}
+
+variable "alert_receiver_url" {
+  default     = ""
+  type        = string
+  description = "API endpoint to send webhook alert requests"
+}
+
+variable "alert_receiver_username" {
+  default     = ""
+  type        = string
+  description = "Username to use with the alert receiver API"
+}
+
+variable "alert_receiver_password" {
+  default     = ""
+  type        = string
+  sensitive   = true
+  description = "Password to use with the alert receiver API"
+}
+
+variable "namespace" {
+  default     = "monitoring"
+  type        = string
+  description = "The namespace to deploy Prometheus."
+}
+
+variable "metrics_retention_duration" {
+  default     = "14d"
+  type        = string
+  description = "The duration to keep metrics."
+}
+
+variable "metrics_scrape_interval" {
+  default     = "60s"
+  type        = string
+  description = "The frequency at which to collect metrics."
+}
+
+variable "prometheus_volume_size" {
+  default     = "10Gi"
+  type        = string
+  description = "The volume size for Prometheus persistent volume."
+}
+
+variable "kubernetes_cluster_name" {
+  default     = "kubernetes"
+  type        = string
+  description = "Add a label to the metrics identify the cluster name."
+}
+
+variable "certificate_issuer_name" {
+  type        = string
+  description = "The cert-manager certificate issuer name."
+}
