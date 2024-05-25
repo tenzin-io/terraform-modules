c4e874a1c52c4576b5f40d40b722f02ed1d4c26d
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2024-05-25 03:18:57 +0000

    Fix value

--- kubernetes/cert-manager/outputs.tf
+++ kubernetes/cert-manager/outputs.tf
@@ -1,4 +1,4 @@
 output "cert_issuer_name" {
-  value       = try(kubernetes_manifest.lets_encrypt_cluster_issuer.0.metadata.0.name, null)
+  value       = try(kubernetes_manifest.lets_encrypt_cluster_issuer.0.manifest.metadata.0.name, null)
   description = "The name of the certificate issuer"
 }
