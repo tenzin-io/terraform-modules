745c2b61a112228916a2f88b5a9ac8aa2731653c
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2024-06-09 17:35:42 +0000

    Spilt runner from runner-set

---
+++ kubernetes/actions-runner-set/main.tf
@@ -0,0 +1,69 @@
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+  }
+}
+
+resource "helm_release" "actions_runner_set" {
+  name             = "arc-runner-set-${var.runner_set_name}"
+  namespace        = var.namespace
+  create_namespace = true
+  repository       = "oci://ghcr.io/actions/actions-runner-controller-charts"
+  chart            = "gha-runner-scale-set"
+  version          = "0.9.2"
+
+  set {
+    name  = "githubConfigUrl"
+    value = var.github_organization_url
+  }
+
+  set {
+    name  = "githubConfigSecret.github_app_id"
+    value = var.github_app_id
+  }
+
+  set {
+    name  = "githubConfigSecret.github_app_installation_id"
+    value = var.github_app_installation_id
+  }
+
+  set {
+    name  = "githubConfigSecret.github_app_private_key"
+    value = var.github_app_private_key
+  }
+
+  set {
+    name  = "runnerScaleSetName"
+    value = var.runner_set_name
+  }
+
+  set {
+    name  = "minRunners"
+    value = var.minimum_runners
+  }
+
+  set {
+    name  = "maxRunners"
+    value = var.maximum_runners
+  }
+
+  set {
+    name  = "template.spec.containers[0].name"
+    value = "runner"
+  }
+
+  set {
+    name  = "template.spec.containers[0].command[0]"
+    value = "/home/runner/run.sh"
+  }
+
+  set {
+    name  = "template.spec.containers[0].image"
+    value = var.runner_image
+  }
+
+}
---
+++ kubernetes/actions-runner-set/variables.tf
@@ -0,0 +1,51 @@
+variable "namespace" {
+  description = "The namespace to deploy the actions-runner-controller"
+  type        = string
+  default     = "actions-runner"
+}
+
+variable "github_organization_url" {
+  description = "The GitHub Organization URL"
+  type        = string
+}
+
+variable "github_app_id" {
+  description = "The GitHub App ID"
+  type        = number
+  sensitive   = true
+}
+
+variable "github_app_installation_id" {
+  description = "The GitHub App Installation ID"
+  type        = number
+  sensitive   = true
+}
+
+variable "github_app_private_key" {
+  description = "The GitHub App Private Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "runner_image" {
+  description = "The image to use for the runner"
+  type        = string
+  default     = "ghcr.io/actions/actions-runner:latest"
+}
+
+variable "runner_set_name" {
+  description = "The name of the runner set"
+  type        = string
+}
+
+variable "minimum_runners" {
+  description = "The minimum number of runners to scale to"
+  type        = number
+  default     = 1
+}
+
+variable "maximum_runners" {
+  description = "The maximum number of runners to scale to"
+  type        = number
+  default     = 5
+}
