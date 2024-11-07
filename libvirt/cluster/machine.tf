23e88426ae5e12a047c43522ae01983c80e46ba7
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2024-11-07 02:02:12 +0000

    Update machine inputs

--- libvirt/cluster/machine.tf
+++ libvirt/cluster/machine.tf
@@ -1,19 +1,13 @@
 module "cluster" {
   count           = var.vm_node_count
   source          = "git::https://github.com/tenzin-io/terraform-modules.git//libvirt/virtual-machine?ref=main"
-  name            = "worker-${count.index}"
+  name            = "${var.cluster_name}-worker-${count.index}"
   datastore_name  = var.datastore_name
   base_volume_id  = var.base_volume_id
   network_id      = libvirt_network.vpc_network.id
-  cpu_count       = 6
-  memory_size_mib = 48 * 1024  // gib
-  disk_size_mib   = 128 * 1024 // gib
   addresses       = [cidrhost(var.vpc_network_cidr, 10 + count.index)]
-  data_disks = {
-    "/dev/vdb" = {
-      disk_size_mib = 350 * 1024 // gib
-      fs_type       = "ext4"
-      mount_path    = "/data"
-    }
-  }
+  cpu_count       = var.vm_cpu_count
+  memory_size_mib = var.vm_memory_size_mib
+  disk_size_mib   = var.vm_disk_size_mib
+  data_disks      = var.vm_data_disks
 }
