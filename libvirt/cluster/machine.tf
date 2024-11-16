1ecf049e9d931fa2c96875ca1d99850c9961b054
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2024-11-16 04:45:52 +0000

    Add cluster

--- libvirt/cluster/machine.tf
+++ libvirt/cluster/machine.tf
@@ -1,16 +1,16 @@
-# module "cluster" {
-#   count           = var.vm_node_count
-#   source          = "git::https://github.com/tenzin-io/terraform-modules.git//libvirt/virtual-machine?ref=main"
-#   name            = "${var.cluster_name}-worker-${count.index}"
-#   datastore_name  = var.datastore_name
-#   base_volume_id  = var.base_volume_id
-#   network_id      = libvirt_network.vpc_network.id
-#   addresses       = [cidrhost(var.vpc_network_cidr, 10 + count.index)]
-#   cpu_count       = var.vm_cpu_count
-#   memory_size_mib = var.vm_memory_size_mib
-#   disk_size_mib   = var.vm_disk_size_mib
-#   data_disks      = var.vm_data_disks
-# }
+module "cluster" {
+  count           = var.vm_node_count
+  source          = "git::https://github.com/tenzin-io/terraform-modules.git//libvirt/virtual-machine?ref=main"
+  name            = "${var.cluster_name}-worker-${count.index}"
+  datastore_name  = var.datastore_name
+  base_volume_id  = var.base_volume_id
+  network_id      = libvirt_network.vpc_network.id
+  addresses       = [cidrhost(var.vpc_network_cidr, 10 + count.index)]
+  cpu_count       = var.vm_cpu_count
+  memory_size_mib = var.vm_memory_size_mib
+  disk_size_mib   = var.vm_disk_size_mib
+  data_disks      = var.vm_data_disks
+}
 
 module "agent" {
   source          = "git::https://github.com/tenzin-io/terraform-modules.git//libvirt/virtual-machine?ref=main"
