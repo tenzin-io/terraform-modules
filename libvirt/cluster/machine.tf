7945791a5192d280936b129bebcc3c338fea5cb8
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2024-11-16 03:51:30 +0000

    Add launch_script

--- libvirt/cluster/machine.tf
+++ libvirt/cluster/machine.tf
@@ -1,4 +1,18 @@
-module "cluster" {
+# module "cluster" {
+#   count           = var.vm_node_count
+#   source          = "git::https://github.com/tenzin-io/terraform-modules.git//libvirt/virtual-machine?ref=main"
+#   name            = "${var.cluster_name}-worker-${count.index}"
+#   datastore_name  = var.datastore_name
+#   base_volume_id  = var.base_volume_id
+#   network_id      = libvirt_network.vpc_network.id
+#   addresses       = [cidrhost(var.vpc_network_cidr, 10 + count.index)]
+#   cpu_count       = var.vm_cpu_count
+#   memory_size_mib = var.vm_memory_size_mib
+#   disk_size_mib   = var.vm_disk_size_mib
+#   data_disks      = var.vm_data_disks
+# }
+
+module "agent" {
   count           = var.vm_node_count
   source          = "git::https://github.com/tenzin-io/terraform-modules.git//libvirt/virtual-machine?ref=main"
   name            = "${var.cluster_name}-worker-${count.index}"
@@ -10,4 +24,7 @@ module "cluster" {
   memory_size_mib = var.vm_memory_size_mib
   disk_size_mib   = var.vm_disk_size_mib
   data_disks      = var.vm_data_disks
+  launch_script = templatefile("${path.module}/templates/agent_script.sh", {
+    agent_key = "1234"
+  })
 }
---
+++ libvirt/cluster/templates/agent_script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "hello ${agent_key}" > /hello
