66c35e49b18742fab48f687b1c22ddb2a8590956
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2024-11-16 04:20:06 +0000

    Add -y flag

--- libvirt/cluster/templates/agent_script.sh
+++ libvirt/cluster/templates/agent_script.sh
@@ -4,4 +4,4 @@
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/noble.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/noble.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
 apt-get update
-apt-get install tailscale
+apt-get install -y tailscale
