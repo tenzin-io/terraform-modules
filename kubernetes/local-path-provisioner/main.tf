4638440dc7df56d4dd1e703d831671cd2a8e269d
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2024-11-16 20:21:17 +0000

    Support shared filesystem

--- kubernetes/local-path-provisioner/main.tf
+++ kubernetes/local-path-provisioner/main.tf
@@ -18,12 +18,22 @@ resource "helm_release" "local_path_provisioner" {
   }
 
   set {
-    name  = "nodePathMap[0].node"
-    value = "DEFAULT_PATH_FOR_NON_LISTED_NODES"
-  }
-
-  set {
-    name  = "nodePathMap[0].paths[0]"
+    name = "sharedFileSystemPath"
     value = var.local_path
   }
+
+  set_list {
+    name = "nodePathMap"
+    value = []
+  }
+
+#  set {
+#    name  = "nodePathMap[0].node"
+#    value = "DEFAULT_PATH_FOR_NON_LISTED_NODES"
+#  }
+#
+#  set {
+#    name  = "nodePathMap[0].paths[0]"
+#    value = var.local_path
+#  }
 }
