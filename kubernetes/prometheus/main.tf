d397ce27576a08fb533efc8f0fa9180a21718652
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2024-11-25 16:46:37 +0000

    Simple Prometheus

--- kubernetes/prometheus/main.tf
+++ kubernetes/prometheus/main.tf
@@ -7,27 +7,23 @@ terraform {
     }
   }
 }
+
+#
+# prometheus
+#
 resource "helm_release" "prometheus" {
   name             = "prometheus"
   chart            = "kube-prometheus-stack"
   repository       = "https://prometheus-community.github.io/helm-charts"
-  version          = "58.7.2"
+  version          = "66.2.2"
   namespace        = var.namespace
   create_namespace = true
   values = [
     templatefile("${path.module}/templates/values.yaml", {
-      enable_alertmanager        = var.enable_alertmanager
-      alert_receiver_name        = var.alert_receiver_name
-      alert_receiver_url         = var.alert_receiver_url
-      alert_receiver_username    = var.alert_receiver_username
-      alert_receiver_password    = var.alert_receiver_password
       prometheus_volume_size     = var.prometheus_volume_size
       metrics_retention_duration = var.metrics_retention_duration
       metrics_scrape_interval    = var.metrics_scrape_interval
       kubernetes_cluster_name    = var.kubernetes_cluster_name
-      prometheus_fqdn            = var.prometheus_fqdn
-      enable_ingress             = var.enable_ingress
-      enable_basic_auth          = var.enable_basic_auth
     })
   ]
 }
@@ -40,7 +36,7 @@ resource "helm_release" "metrics_server" {
   depends_on = [helm_release.prometheus]
   name       = "metrics-server"
   chart      = "metrics-server"
-  version    = "3.11.0"
+  version    = "3.12.2"
   repository = "https://kubernetes-sigs.github.io/metrics-server/"
   namespace  = var.namespace
 
@@ -52,16 +48,4 @@ resource "helm_release" "metrics_server" {
     name  = "args"
     value = "{--kubelet-insecure-tls}"
   }
-}
-
-resource "kubernetes_secret_v1" "basic_auth" {
-  count = var.enable_basic_auth ? 1 : 0
-  metadata {
-    name      = "basic-auth"
-    namespace = var.namespace
-  }
-  data = {
-    auth = var.basic_auth_password
-  }
-  depends_on = [helm_release.prometheus]
 }
--- kubernetes/prometheus/variables.tf
+++ kubernetes/prometheus/variables.tf
@@ -1,34 +1,3 @@
-variable "enable_alertmanager" {
-  default     = false
-  type        = bool
-  description = "Enable Alertmanager"
-}
-
-variable "alert_receiver_name" {
-  default     = ""
-  type        = string
-  description = "Name of the alertmanager receiver"
-}
-
-variable "alert_receiver_url" {
-  default     = ""
-  type        = string
-  description = "API endpoint to send webhook alert requests"
-}
-
-variable "alert_receiver_username" {
-  default     = ""
-  type        = string
-  description = "Username to use with the alert receiver API"
-}
-
-variable "alert_receiver_password" {
-  default     = ""
-  type        = string
-  sensitive   = true
-  description = "Password to use with the alert receiver API"
-}
-
 variable "namespace" {
   default     = "monitoring"
   type        = string
@@ -57,28 +26,4 @@ variable "kubernetes_cluster_name" {
   default     = "kubernetes"
   type        = string
   description = "Add a label to the metrics identify the cluster name."
-}
-
-variable "prometheus_fqdn" {
-  default     = "prometheus.lan"
-  type        = string
-  description = "The Prometheus DNS name to place on ingress."
-}
-
-variable "enable_ingress" {
-  type        = bool
-  default     = false
-  description = "Create a Prometheus ingress."
-}
-
-variable "enable_basic_auth" {
-  type        = bool
-  default     = false
-  description = "Protect ingress using basic auth."
-}
-
-variable "basic_auth_password" {
-  type        = string
-  default     = null
-  description = "The basic auth credentials."
 }
