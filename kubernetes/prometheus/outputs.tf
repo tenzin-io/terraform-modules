0b8b72572defd1aa2ef1ef6846a91541ce01288a
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2024-12-07 16:51:11 +0000

    Update prometheus module

--- kubernetes/prometheus/outputs.tf
+++ kubernetes/prometheus/outputs.tf
@@ -1,3 +1,7 @@
 output "prometheus_url" {
   value = "http://prometheus-kube-prometheus-prometheus.${var.namespace}.svc.cluster.local:9090"
 }
+
+output "alertmanager_url" {
+  value = "http://alertmanager-operated.${var.namespace}.svc.cluster.local:9093"
+}
