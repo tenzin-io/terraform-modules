6d65d5a9509d1d67d1e0d410c0746ca2ec1d5de3
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2024-12-21 22:56:48 +0000

    Should be a comment

--- libvirt/cluster/templates/agent_script.sh
+++ libvirt/cluster/templates/agent_script.sh
@@ -4,7 +4,7 @@
 echo 'Acquire::Retries "5";' > /etc/apt/apt.conf.d/80-retries
 
 # setup tailscale
-install tailscale - https://tailscale.com/download/linux
+# install tailscale - https://tailscale.com/download/linux
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/noble.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/noble.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
 apt-get update
